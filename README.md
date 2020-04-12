80890d176518ec25a9b8145fde404bc7a62e2105
Johannes  Kruse <johanneskruse@Johanness-MacBook-Pro-2.local>
2020-04-12 22:36:34 +0200

    image_scraper complete

--- README.md
+++ README.md
@@ -7,34 +7,34 @@ python config.py
 ```
 
 # Preparation: 
+## Download images for the experiment: 
+First we download all images needed for feature extraction: 
+
+```
+python scripts/image_scraper.py -fname "data/proposals2.npz" -url_name "url"
+```
+
+This process can easily take an hour. For the 5k sample size we experinced an error for 10 of the urls. Since this is a significantly small amount of images, these samples will simple be removed at a later state when the images features are merged in "" (script). 
+
+## Download pre-trained model: 
+To download the pre-trained fastText "da" model download the binary file: https://fasttext.cc/docs/en/pretrained-vectors.html (bin+text)
+
+## Generate dataset with word embeddings:
 We use the python package called ‘fasttext’ which introduces high level interface to use the vector files along with some other fastText functionalities:
 
 ```
 pip install fasttext
 ```
 
-## Download pre-trained model: 
-To download the pre-trained fastText "da" model download the binary file: https://fasttext.cc/docs/en/pretrained-vectors.html (bin+text)
-
-## Generate dataset with word embeddings:
 To generate the data with word embeddings: 
 ```
 python scripts/word_embedding.py -fname "data/proposals2.npz" -model "wiki.da/wiki.da.bin"
 ```
 
-## Download images for the experiment: 
-To download all images needed for feature extraction: 
 
-```
-pip install grequests
-```
 
-```
-python scripts/image_scraper.py -fname "data/proposals2.npz" -url_name "url"
-```
 
 
 # Dependencies: 
-- gensim: 3.8.1
 - request: 2.22.0
 - urllib.request: 3.7
--- scripts/image_scraper.py
+++ scripts/image_scraper.py
@@ -1,16 +1,11 @@
 # ============================================================================================================
 # import packages
-from bs4 import BeautifulSoup as bs
 import requests
 import urllib.request
 import os
 import argparse
 from config import *
 import numpy as np
-
-import os
-from pathlib import Path
-
 
 # ============================================================================================================
 #Define the argument parser to read in the URL
@@ -19,18 +14,28 @@ parser.add_argument('-fname', default="data/proposals2.npz", help='File with ima
 parser.add_argument('-url_name', type=str, default="url", help='Name of the title for the URL')
 args = vars(parser.parse_args())
 
+
 f_name = args['fname']
 url_name = args['url_name']
 
+# # For debug: 
 # f_name = "data/proposals2.npz"
 # url_name = "url"
+
 # ============================================================================================================
 
 
 # Function to take an image url and save the image in the given directory
 def download_image(url, save_path):
+    # Find png file name: 
     name = str(url.split('/')[-1])
+    # Save image:
     urllib.request.urlretrieve(url,os.path.join(save_path, name))
+
+
+def image_name(url):
+    name = str(url.split('/')[-1])
+    return name
 
 
 # ============================================================================================================
@@ -39,7 +44,7 @@ def download_image(url, save_path):
 local_files = Config.local_path
 
 # Create the directory name where the images will be saved
-dir_img = local_files / "data" / "images"
+dir_img = local_files / "images"
 # Create the directory if already not there
 if not os.path.exists(dir_img):
     os.mkdir(dir_img)
@@ -49,7 +54,6 @@ dir_data = local_files / f_name
 data = np.load(dir_data, encoding="latin1")
 
 # ============================================================================================================
-
 links = data[url_name]
 error_img = []
 
@@ -58,36 +62,8 @@ for i, img in enumerate(links):
         download_image(img, dir_img)
     except:
         error_img.append([i, img])
-        print(f"Error for image: {i}")
-
-    # Print the number of images
+        print(f"Error for image: {i}")    
     if i % 50 == 0: 
         print(f"[INFO] Downloaded {i} of {len(links)} images.")
 
-
-# # Synchronous
-# for link in links:
-#     req = requests.get(link)
-#     soup = BeautifulSoup(req.text, 'lxml')
-#     lists = soup.find_all('a', attrs={'class':"product__list--name"})
-#     print(lists[0].text)
-#     prices = soup.find_all('span', attrs={'class':"pdpPriceMrp"})
-#     print(prices[0].text)
-#     discount = soup.find_all("div", attrs={"class":"listingDiscnt"})
-#     print(discount[0].text)
-
-
-# import grequests
-
-# reqs = (grequests.get(link) for link in links)
-# resp = grequests.imap(reqs, grequests.Pool(10))
-
-# for r in resp:
-#     soup = BeautifulSoup(r.text, 'lxml')
-#     results = soup.find_all('a', attrs={"class":'product__list-name'})
-#     print(results[0].text)
-#     prices = soup.find_all('span', attrs={'class':"pdpPriceMrp"})
-#     print(prices[0].text)
-#     discount = soup.find_all("div", attrs={"class":"listingDiscnt"})
-#     print(discount[0].text)
- 
+print(f"[INFO] Downloaded {i+1} of {len(links)} images.")
