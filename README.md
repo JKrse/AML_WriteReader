9eeba342c47a2b397c3b71e3f00392497ff0f110
Johannes  Kruse <johanneskruse@Johanness-MacBook-Pro-2.local>
2020-04-14 13:47:54 +0200

    Image feature extraction added

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ python config.py
 ```
 
 # Preparation: 
+Downloading and performing feature extraction for all images takes quite a lot of time 2-3 hours. Hence, to ease this process we recommend downloading preprocessed dataset (LINK)
+
+To manully do this please follow the insctructions: 
 ## Download images for the experiment: 
 First we download all images needed for feature extraction: 
 
@@ -15,6 +18,19 @@ python scripts/image_scraper.py -fname "data/proposals2.npz" -url_name "url"
 ```
 
 This process can easily take an hour. For the 5k sample size we experinced an error for 10 of the urls. Since this is a significantly small amount of images, these samples will simple be removed at a later state when the images features are merged in "" (script). 
+
+## Feature extraxtion 
+Having all the images downloaded the next step is to perfom feature extraction for each image. 
+
+```
+pip install opencv-python
+```
+
+To get the features from each image: 
+```
+python scripts/image_feature_extraction.py -fname "./local_files/images"
+```
+This process takes 1-2 hours, hence we recommend downloading the preprossed data: (LINK).
 
 ## Download pre-trained model: 
 To download the pre-trained fastText "da" model download the binary file: https://fasttext.cc/docs/en/pretrained-vectors.html (bin+text)
@@ -31,6 +47,7 @@ To generate the data with word embeddings:
 python scripts/word_embedding.py -fname "data/proposals2.npz" -model "wiki.da/wiki.da.bin"
 ```
 
+## Prepare the final preprossed dataset: 
 
 
 
@@ -38,3 +55,4 @@ python scripts/word_embedding.py -fname "data/proposals2.npz" -model "wiki.da/wi
 # Dependencies: 
 - request: 2.22.0
 - urllib.request: 3.7
+- keras-2.3.1
---
+++ scripts/image_feature_extraction.py
@@ -0,0 +1,61 @@
+# ============================================================================================================
+import keras
+from config import *
+import os
+from cv2 import cv2
+import numpy as np
+import argparse
+# from keras.datasets import cifar10 # (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# ============================================================================================================
+local_path = Config.local_path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-fname', default=f"./{local_path}/images", help='Path to folder(s) with images')
+args = vars(parser.parse_args())
+
+f_name = args['fname']
+
+# ============================================================================================================
+
+# Load Keras ResNet model: (Add to config - models section)
+model = keras.applications.resnet.ResNet152(include_top=False, weights='imagenet', input_tensor=None, 
+                                            input_shape=None, pooling='avg', classes=1000)
+
+
+# ============================================================================================================
+
+# rootdir = f"{Config.local_path_temp}/images"
+rootdir = f_name
+
+img_name = []
+img_feat = []
+
+for dirpath, dirnames, filenames in os.walk(rootdir):
+    for i, file in enumerate(filenames):
+        try:
+            # Load images:
+            img_path = os.path.join(dirpath, file)   
+            img = cv2.imread(img_path)   
+
+            # Images shape: [batch_size, image_width, image_height, number_of_channels]
+            shape = img.shape
+
+            # Feature extraction:
+            features = model.predict(img.reshape(1, shape[0], shape[1], shape[2]))
+
+            # Append features and file name: 
+            img_name.append(file)
+            img_feat.append(features)
+
+            if i % 50 == 0: 
+                print(f"[INFO] {i} of {len(filenames)} images have been feature extracted.")    
+        except:
+            print(f"Error: {file}")
+    print(f"[INFO] {i+1} of {len(filenames)} images have been feature extracted.")
+
+img_feat_dict = {"Images": img_name, "Image_features": img_feat}
+
+# Saving Data: 
+print(f"[INFO] Saving: {local_path}/data/image_features.npz")
+np.savez(f"{local_path}/data/image_features.npz",**img_feat_dict)
